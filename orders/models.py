2f8abadf527b27dcca5827fe265a339115c04381
Aurian Nogues <aurian.nogues@gmail.com>
2019-02-19 23:44:13 +0000

    started creating models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Pasta(models.Model):
+    description = models.CharField(max_length=64)
+    price = models.FloatField(max_length=8)
+    
+    def __str__(self):
+        return f"{self.id} - {self.description} : {self.price}"
+
+class Categories(models.Model):
+    flavour = models.CharField(max_length=16)
+    size = models.CharField(max_length=16)
+    toppingType = models.CharField(max_length=32)
+
+    def __str__(self):
+        return f"{self.id} - {self.flavour} / {self.size} / {self.toppingType}"
+
+
+
+      
