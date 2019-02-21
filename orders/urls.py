a31a1e2fc1842a798a12b7f92762507c316d2ef1
Aurian Nogues <aurian.nogues@gmail.com>
2019-02-21 12:02:53 +0000

    login ok

--- orders/urls.py
+++ orders/urls.py
@@ -4,6 +4,16 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout")
+]
+
+
+
+
+""" urlpatterns = [
+    path("", views.index, name="index"),
     path("<int:pizza_id>", views.pizza, name="pizza")
 
 ]
+ """
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,46 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from .models import Pasta, Pizza
+from django.urls import reverse
 
 # Create your views here.
+
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "orders/login.html", {"message": None})
+    context = {
+        "user": request.user
+    }
+    return render(request, "orders/menu.html", context)
+
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "orders/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "orders/login.html", {"message": "Logged out."})
+
+
+
+
+
+
+
+
+
+
+
+
+""" def index(request):
     context = {
         "Pastas": Pasta.objects.all()
     }
@@ -17,4 +54,4 @@ def pizza(request, pizza_id):
     context = {
         "pizza": pizza
     }
-    return render(request, "orders/pizza.html", context)
+    return render(request, "orders/pizza.html", context) """
