5ed538157c1add021216a5e6ca3972af94912cab
Aurian Nogues <aurian.nogues@gmail.com>
2019-02-24 19:52:33 +0000

    basic button functionalities

--- orders/static/orders/menu.js
+++ orders/static/orders/menu.js
@@ -1,9 +1,22 @@
 //////////////Wrapper for to be executed only when page finished loading
 document.addEventListener('DOMContentLoaded', () => {
-    $(document).on("click",".pizza-button",test);
+    $(document).on("click",".pizza-button",add_pizza);
 
 });
 
-function test(){
-    alert("I am here");
+function add_pizza(){
+    var price = $(this).closest('td').prev('td');
+    var topping = $(price).closest('td').prev('td');
+    var size = $(topping).closest('td').prev('td');
+
+    type= "standard pizza"
+    price = price.text();
+    price = parseFloat (price);
+    topping = topping.text();
+    size = size.text();
+
+    console.log(type);
+    console.log(price);
+    console.log(topping);
+    console.log(size);
 }
