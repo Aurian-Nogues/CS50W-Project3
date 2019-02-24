22cb24ffc59189a5bf55aa77979b8e43a2b6b805
Aurian Nogues <aurian.nogues@gmail.com>
2019-02-24 19:13:54 +0000

    added buttons

--- orders/static/orders/menu.js
+++ orders/static/orders/menu.js
@@ -1,5 +1,9 @@
 //////////////Wrapper for to be executed only when page finished loading
 document.addEventListener('DOMContentLoaded', () => {
-    //alert("document loaded");
+    $(document).on("click",".pizza-button",test);
 
 });
+
+function test(){
+    alert("I am here");
+}
